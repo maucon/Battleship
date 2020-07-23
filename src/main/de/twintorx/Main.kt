5ca7f657ec4eef33f559b155b946aca5ba363b40
maucon <maucon13@gmail.com>
2020-07-23 16:32:00 +0200

    added GameBoard

--- src/main/de/twintorx/Main.kt
+++ src/main/de/twintorx/Main.kt
@@ -2,9 +2,19 @@ package main.de.twintorx
 
 import main.de.twintorx.console.Color
 import main.de.twintorx.console.print
+import main.de.twintorx.game.GameBoard
 
 fun main() {
     Color.values().forEach {
         print("$it: DESTROY", it)
     }
+
+    val gameBoard = GameBoard()
+    (0..9).forEach {
+        gameBoard.hit(it, (0..9).random())
+        gameBoard.hit(it, (0..9).random())
+        gameBoard.hit(it, (0..9).random())
+    }
+    
+    print(gameBoard)
 }
---
+++ src/main/de/twintorx/game/GameBoard.kt
@@ -0,0 +1,26 @@
+package main.de.twintorx.game
+
+import main.de.twintorx.console.Color
+
+class GameBoard(
+        val size: Int = 10
+) {
+    private val grid: Array<Array<Boolean>> = Array(10) { Array(10) { false } }
+
+    fun hit(x: Int, y: Int) {
+        grid[size - y - 1][x] = true
+    }
+
+    override fun toString(): String {
+        var print = ""
+
+        grid.forEach {
+            it.forEach { cell ->
+                print += (if (cell) Color.RED.ansi + "1" else Color.GREEN.ansi + "0") + " "
+            }
+            print += "\n"
+        }
+
+        return print
+    }
+}
