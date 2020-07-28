bf84d8f6061cf540b7dbd68fa2955cec7facc8c2
maucon <maucon13@gmail.com>
2020-07-28 10:07:17 +0200

    edited Ship

--- src/main/de/twintorx/battleship/game/Ship.kt
+++ src/main/de/twintorx/battleship/game/Ship.kt
@@ -2,10 +2,31 @@ package main.de.twintorx.battleship.game
 
 class Ship(
         val name: String,
-        val length: Int,
-        private var health: Int
+        val length: Int
 ) {
-    fun hit() {
-        --health
+    private var health: Int = length
+
+    fun hit() = --health
+}
+
+fun getStandardShipSet(): Collection<Ship> {
+    val list = arrayListOf(Ship("Carrier", 5))
+    list add Ship("Battleship", 4) * 2
+    list add Ship("Cruiser", 3) * 3
+    list add Ship("Submarine", 3) * 4
+    list add Ship("Destroyer", 2) * 5
+    return list
+}
+
+// ---------------- Extensions and Overloading ----------------
+private operator fun Ship.times(i: Int): Collection<Ship> {
+    val list = ArrayList<Ship>()
+    (1..i).forEach { _ ->
+        list.add(Ship(this.name, this.length))
     }
+    return list
+}
+
+private infix fun ArrayList<Ship>.add(ships: Collection<Ship>) {
+    this.addAll(ships)
 }
