d99a0de77e625b77ea8783303b837b43078fe93f
maucon <maucon13@gmail.com>
2020-07-29 08:09:58 +0200

    improved readability TrackBoard toString

--- src/main/de/twintorx/battleship/game/TrackBoard.kt
+++ src/main/de/twintorx/battleship/game/TrackBoard.kt
@@ -26,24 +26,22 @@ open class TrackBoard(
 
         grid.values.withIndex().forEach {
             val index = size - it.index
+            val indexPadding = sizeLength - index.toString().length
 
-            table add "${" " * (sizeLength - index.toString().length)}$index │" +
+            table.add("${" " * indexPadding}$index │" +
                     "${it.value.joinToString("│") { cell ->
                         " ${cell.value} "
-                    }}│" add div
+                    }}│")
+            table.add(div)
         }
 
         table.removeLast()
-        return (table add " $space└${"───┴" * len}───┘"
-                add "   $space${(65..(64 + size)).map { it.toChar() }.joinToString("   ")}")
-                .joinToString("\n")
+        table.add(" $space└${"───┴" * len}───┘")
+        table.add("   $space${(65..(64 + size)).map { it.toChar() }.joinToString("   ")}")
+
+        return table.joinToString("\n")
     }
 }
 
 // ---------------- Extensions and Overloading ----------------
 private operator fun String.times(i: Int): String = this.repeat(i)
-
-private infix fun ArrayList<String>.add(element: String): ArrayList<String> {
-    add(element)
-    return this
-}
