6532fd57e703261a2b9f6ee89e7c64d526688ab8
gebauer <Dennis.Gebauer@adesso.de>
2020-08-04 13:04:10 +0200

    -added shadow -added jansi

--- src/main/java/de/twintorx/battleship/Main.java
+++ src/main/java/de/twintorx/battleship/Main.java
@@ -1,9 +1,18 @@
 package de.twintorx.battleship;
 
-import de.twintorx.battleship.game.Player;
+import org.fusesource.jansi.AnsiConsole;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class Main {
-    public static void main(String[] args) {
-        new Player().connect();
+    public static void main(String[] args) throws InterruptedException {
+        if (System.console() != null && System.getenv().get("OS").contains("Windows")) {
+            AnsiConsole.systemInstall(); // only needed for windows
+        }
+        System.out.println(ansi().eraseScreen().render("@|red Hello|@ @|green World|@")); // TODO erase screen clears screen
+//        new Player().connect();
+        if (System.console() != null && System.getenv().get("OS").contains("Windows")) {
+            AnsiConsole.systemUninstall(); // only needed for windows
+        }
     }
 }
